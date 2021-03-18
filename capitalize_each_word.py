998b4588e01e775652e997096362e8d2aee4604a
Jkverma1 <66208658+Jkverma1@users.noreply.github.com>
2021-03-18 20:19:07 +0530

    Update capitalize_each_word.py

--- capitalize_each_word.py
+++ capitalize_each_word.py
@@ -1,10 +1,10 @@
 # str "this is my first program"
 f = open("file.txt","r")
 content = f.read()
-newcontent = content.split()
+content = content.split()
 print(newcontent)
 result = str()
-for  i in newcontent:
+for  i in content:
     result+= i.capitalize()
     result+=" "
 print(result)
